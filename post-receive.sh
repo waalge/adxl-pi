a29767c6d7707c40268d59ef63b0007da99033b3
waalge <waalgy@gmail.com>
2020-09-27 11:49:36 +0100

    Add Makefile and post-receive.sh.
    Do amend

---
+++ post-receive.sh
@@ -0,0 +1,14 @@
+TARGET="/home/pi/sandpit/adxl-pi"
+GIT_DIR="/home/pi/repo/adxl-pi.git"
+BRANCH="build"
+
+ref=$1
+if [ "$ref" = "refs/heads/$BRANCH" ];
+then
+	echo "Ref $ref received. Deploying ${BRANCH} branch to production..."
+	git --work-tree=$TARGET --git-dir=$GIT_DIR checkout -f $BRANCH
+	cd $TARGET
+	make
+else
+	echo "Ref $ref received. Doing nothing: only the ${BRANCH} branch may be deployed on this server."
+fi
