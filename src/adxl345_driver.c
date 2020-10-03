ffbade450255af8081fafd7417fbdc9d5e0c12fc
waalge <waalgy@gmail.com>
2020-10-03 11:06:31 +0100

    test

--- src/adxl345_driver.c
+++ src/adxl345_driver.c
@@ -15,7 +15,9 @@ int writeAdxlBytes(int handle, char *data, int count) {
 }
 
 int setAdxlRegister(int handle, char address, char dat) {
-    char * data = {address, dat};
+    char * data[2];
+    data[0] = address;
+    data[1] = dat;
     return spiWrite(handle, data, 2);
 }
 
