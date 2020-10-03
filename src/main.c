5d6f75f66a2c3baeaf5eede1aeb4de4e79f40d73
waalge <waalgy@gmail.com>
2020-10-03 15:44:14 +0100

    test

--- src/main.c
+++ src/main.c
@@ -24,6 +24,12 @@ int setupAdxl(int spiSpeed, adxl conf) {
   return h;
 }
 
+void print_arr(char* arr, int arrLen) {
+  for (int loop = 0; loop < arrLen; loop++) {
+    printf("%d ", arr[loop]);
+  }
+}
+
 void getReading(int h, int16_t *x) {
   int dataLen = 8;
   char data[dataLen];
