ecbe392065787938b2f714c6942130cf86cf4f7e
waalge <waalgy@gmail.com>
2020-10-03 15:33:14 +0100

    test

--- src/adxl345_driver.c
+++ src/adxl345_driver.c
@@ -1,35 +1,33 @@
 #include "adxl345_driver.h"
 
-#define READ_BIT      0x80
-#define MULTI_BIT     0x40
+#define READ_BIT 0x80
+#define MULTI_BIT 0x40
 
 int readAdxlBytes(int handle, char *data, int count) {
-    data[0] |= READ_BIT;
-    if (count > 1) data[0] |= MULTI_BIT;
-    return spiXfer(handle, data, data, count);
+  data[0] |= READ_BIT;
+  if (count > 1)
+    data[0] |= MULTI_BIT;
+  return spiXfer(handle, data, data, count);
 }
 
 int writeAdxlBytes(int handle, char *data, int count) {
-    if (count > 1) data[0] |= MULTI_BIT;
-    return spiWrite(handle, data, count);
+  if (count > 1)
+    data[0] |= MULTI_BIT;
+  return spiWrite(handle, data, count);
 }
 
 int setAdxlRegister(int handle, char address, char dat) {
-    char data[] = {address, dat};
-    return spiWrite(handle, data, 2);
+  char data[] = {address, dat};
+  return spiWrite(handle, data, 2);
 }
-
 
 int openAdxl(int spiSpeed) {
-    gpioInitialise();
-    if (gpioInitialise() < 0) {
-        printf("Failed to initialize GPIO!");
-        return 1;
-    }
-    return spiOpen(0, spiSpeed, 3);
+  gpioInitialise();
+  if (gpioInitialise() < 0) {
+    printf("Failed to initialize GPIO!");
+    return 1;
+  }
+  return spiOpen(0, spiSpeed, 3);
 }
 
-void teardownAdxl() {
-    return gpioTerminate();
-}
-
+void teardownAdxl() { return gpioTerminate(); }
--- src/main.c
+++ src/main.c
@@ -1,77 +1,76 @@
-#include <stdio.h>
-#include <pigpio.h>
-#include <time.h>
 #include <math.h>
-#include <string.h>
+#include <pigpio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "adxl345_driver.h"
 
-const int spiSpeed = 2000000;  // SPI communication speed, bps
+const int spiSpeed = 2000000; // SPI communication speed, bps
 
 typedef struct {
-	char bwRate;
-	char dataFormat;
-	char powerCtl;
-	char fifoCtl;
+  char bwRate;
+  char dataFormat;
+  char powerCtl;
+  char fifoCtl;
 } adxl;
 
 int setupAdxl(int spiSpeed, adxl conf) {
-    int h = openAdxl(spiSpeed);
-    setAdxlRegister(h, BW_RATE, conf.bwRate);
-    setAdxlRegister(h, DATA_FORMAT, conf.dataFormat);
-    setAdxlRegister(h, POWER_CTL, conf.powerCtl);
-    return h;
+  int h = openAdxl(spiSpeed);
+  setAdxlRegister(h, BW_RATE, conf.bwRate);
+  setAdxlRegister(h, DATA_FORMAT, conf.dataFormat);
+  setAdxlRegister(h, POWER_CTL, conf.powerCtl);
+  return h;
 }
 
-void getReading(int h, int16_t * x) {
-    int dataLen = 8;
-    char data[dataLen];
-    data[0] = DATAX0;
-    int bytes = readAdxlBytes(h, data, dataLen);
-    if (bytes != dataLen) {
+void getReading(int h, int16_t *x) {
+  int dataLen = 8;
+  char data[dataLen];
+  data[0] = DATAX0;
+  int bytes = readAdxlBytes(h, data, dataLen);
+  if (bytes != dataLen) {
     printf("Error occurred!");
-    }
-    x[0] = ((int)data[2]<<8)|(int)data[1];
-    x[1] = ((int)data[4]<<8)|(int)data[3];
-    x[2] = ((int)data[6]<<8)|(int)data[5];
+  }
+  x[0] = ((int)data[2] << 8) | (int)data[1];
+  x[1] = ((int)data[4] << 8) | (int)data[3];
+  x[2] = ((int)data[6] << 8) | (int)data[5];
 }
-
 
 int main() {
-    // SPI sensor setup
-    adxl conf;
-    conf.bwRate = RATE_3200_HZ;
-    conf.dataFormat = (RANGE_PM_2g & 3);
-    conf.powerCtl = PCTL_MEASURE;
-    int h = setupAdxl(spiSpeed, conf);
-    int dataLen = 7;
-    char data[dataLen];
-    int16_t x[3];
-    for (int loop = 0; loop < 10; loop++) {
+  // SPI sensor setup
+  adxl conf;
+  conf.bwRate = RATE_3200_HZ;
+  conf.dataFormat = (RANGE_PM_2g & 3);
+  conf.powerCtl = PCTL_MEASURE;
+  int h = setupAdxl(spiSpeed, conf);
+
+  int dataLen = 7;
+  char data[dataLen];
+  int16_t x[3];
+  for (int loop = 0; loop < 10; loop++) {
     getReading(h, x);
     printf("X,Y,Z %d %d %d\n", x[0], x[1], x[2]);
-    }
-    getReading(h, x);
-    int success = 1;
-    int bytes;
-    data[0] = DATAX0;
-    bytes = readAdxlBytes(h, data, dataLen);
-    if (bytes != dataLen) {
-	success = 0;
-    }
+  }
+  getReading(h, x);
+  int success = 1;
+  int bytes;
+  data[0] = DATAX0;
+  bytes = readAdxlBytes(h, data, dataLen);
+  if (bytes != dataLen) {
+    success = 0;
+  }
 
-if (success == 0) {
+  if (success == 0) {
     printf("Error occurred!");
     return 1;
-}
-   printf("X,Y,Z %d %d %d\n", x[0], x[1], x[2]);
+  }
+  printf("X,Y,Z %d %d %d\n", x[0], x[1], x[2]);
 
-   for(int loop = 0; loop < dataLen; loop++) {
-      printf("%d ", data[loop]);
-   }
-    printf("Done ;( .\n");
-    return 0;
-    teardownAdxl();
-
+  for (int loop = 0; loop < dataLen; loop++) {
+    printf("%d ", data[loop]);
+  }
+  teardownAdxl();
+  printf("Done ;( .\n");
+  return 0;
 }
