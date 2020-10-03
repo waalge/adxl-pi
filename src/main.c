47f201e63455b6d90f488400e9516262686c7664
waalge <waalgy@gmail.com>
2020-10-03 11:54:23 +0100

    test

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ void getReading(int h, int16_t * x) {
 int main() {
     // SPI sensor setup
     adxl conf;
-    conf.bwRate = RATE_800_HZ;
+    conf.bwRate = RATE_50_HZ;
     conf.dataFormat = JUSTIFY || RANGE_PM_2g;
     conf.powerCtl = PCTL_MEASURE;
     int h = setupAdxl(spiSpeed, conf); 
